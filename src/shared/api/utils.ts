d147ae92ba18d267bd53a75eaf509ac95330a938
memento mori <Daniil.Ocheretin@lemanapro.ru>
2026-01-20 17:29:27 +0300

    feat: [TASK-03] fix env for api

--- src/shared/api/utils.ts
+++ src/shared/api/utils.ts
@@ -1,5 +1,3 @@
 export const getBaseUrl = (version = "v1") => {
-  // return `http://localhost:3001/${version}`;
-
-  return `${process.env["NEXT_PUBLIC_API_HOST"]}/${version}`;
+  return `${process.env["NEXT_PUBLIC_APP_URL"]}/${version}`;
 };
